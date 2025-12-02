9fc5d015b303e23d6379d9e9f2a3c8198988de1a
Boliggiganten <joakim.a.ludvigsen@icloud.com>
2025-12-02 17:54:30 +0100

    Delete meshy_github_ready directory

--- meshy_github_ready/netlify/functions/redesignRoom.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async function(event, context) {
-  try {
-    const body = JSON.parse(event.body || '{}');
-    return { statusCode: 200, body: JSON.stringify({ success:true, redesignedImageUrl: null, external: {} }) };
-  } catch(err) {
-    return { statusCode: 500, body: JSON.stringify({ error: String(err) }) };
-  }
-};
